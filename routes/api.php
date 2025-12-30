615162fe0f1c64992cd63df71b300f5f3c00b16e
RubenAlfandoWhite <rubenalfandowhite_2428250004@mhs.mdp.ac.id>
2025-12-30 12:20:45 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonPredictController;
 
-Route::middleware('auth:sanctum')->post(
-    '/predict-pokemon',
-    [PokemonPredictController::class, 'predict']
-);
+Route::post('/predict-pokemon', [PokemonPredictController::class, 'predict']);
+
 
