df1059316bf0a724bc72d99bdf61d0b79f30a441
Nicholas Raphael <nicholasraphaelvalentiuslaw_2428250016@mhs.mdp.ac.id>
2026-01-02 17:24:17 +0700

    gooo

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonPredictController;
 
 Route::post('/predict-pokemon', [PokemonPredictController::class, 'predict']);
-
-
